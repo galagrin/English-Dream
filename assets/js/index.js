e66087c06301a7370baf9ef465e215afb20522dd
nastushka-15 <nastushka-15@yandex.ru>
2024-04-26 00:45:27 +0300

    position fixed в меню

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,18 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function() { // Добавление полупрозрачного фона при прокрутке 
     if (scrollY > 50) {
       document.querySelector('.header__logo-image-container').classList.add('top-of-page')
     } else {
       document.querySelector('.header__logo-image-container').classList.remove('top-of-page')
     }
   })
+
+  // const stickyDiv = document.querySelector('.header__logo-image-container '); // Выбираем необходимый div
+  // const stickyPoint = stickyDiv.offsetTop; // Точка, при достижении которой элемент становится фиксированным
+
+  // window.addEventListener('scroll', () => {
+  //   if (window.pageYOffset >= stickyPoint) {
+  //     stickyDiv.classList.add('sticky-top'); // Фиксируем элемент, когда прокрутили до нужной точки
+  //   } else {
+  //     stickyDiv.classList.remove('sticky-top'); // Освобождаем его, если скролл находится выше этой точки
+  //   }
+  // });
